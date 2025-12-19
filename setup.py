543a074c6075af383123aca7dce2cddd1ed73c35
Bence Popovics <47786942+popbence@users.noreply.github.com>
2025-12-19 17:24:45 +0100

    Bump version from 0.1.1 to 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gPCE_model',
-    version='0.1.1',
+    version='0.1.3',
     author='András Urbanics, Bence Popovics, Emese Vastag, Elmar Zander, Noémi Friedman',
     author_email='popbence@hun-ren.sztaki.hu',
     description='Implementing generalized Polynomial Chaos Expansion (gPCE) for uncertainty quantification and surrogate modeling',
