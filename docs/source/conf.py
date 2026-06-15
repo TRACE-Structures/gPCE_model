2b79d808a730fcdfbad4aa139d84009d42d84a38
Bence Popovics <47786942+popbence@users.noreply.github.com>
2026-06-15 21:46:16 +0200

    Update Sphinx configuration for documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,11 +21,18 @@ release = 'v0.1.11'
 
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',  # For Google and NumPy style docstrings
-    'sphinx.ext.viewcode'   # Optional: to include links to source code
+    'numpydoc',
+    'sphinx_copybutton',
+    'sphinx_design',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode',
+    'IPython.sphinxext.ipython_console_highlighting',
+    'IPython.sphinxext.ipython_directive',
 ]
 
-napoleon_numpy_docstring = True
+numpydoc_show_class_members = False
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -35,5 +42,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
+
+coverage_show_missing_items = True
