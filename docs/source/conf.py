f8783960c50c9ffb8ef42cfe75cf996a03576f10
Bence Popovics <47786942+popbence@users.noreply.github.com>
2026-06-15 21:13:18 +0200

    Update Sphinx configuration for documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,6 +25,8 @@ extensions = [
     'sphinx.ext.viewcode'   # Optional: to include links to source code
 ]
 
+napoleon_numpy_docstring = True
+
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -33,5 +35,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'shpinx_rtd_theme'
 html_static_path = ['_static']
